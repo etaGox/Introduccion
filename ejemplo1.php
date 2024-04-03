a2407130ad212dd383c3b75d616f4b08ffdbed33
Gonzalo Garcia <gonzalogg21@hotmail.com>
2024-04-03 09:01:54 -0300

    Metimos un par de variables al codigo

--- ejemplo1.php
+++ ejemplo1.php
@@ -11,6 +11,10 @@
         #FNAF es god
         /*FNAF tiene lore god*/
         //FNAF tiene juegos god
+        $nombre = 'Manolo';
+        $edad = 31;
+        $altura =1.72;
+        $mayorEdad = true;
         echo <<<END
         No sé si lo hice bien, pero llamé
         No me gustó lo que eschuché.
