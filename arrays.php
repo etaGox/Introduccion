029d0b7de16139ec54ab5a7e9da2f0d2fc39420b
Gonzalo Garcia <gonzalogg21@hotmail.com>
2024-04-24 08:57:11 -0300

    Completamos el archivo con mas arrays y nuevos comandos

--- arrays.php
+++ arrays.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,18 +6,39 @@
 </head>
 <body>
     <?php 
-    $semana = [
-    'Lunes',
-    'Martes',
-    'Miercoles',
-    'Jueves',
-    'Viernes',
-    'Sabado',
-    'Domingo'
-    ];
-    
+    /*
     var_dump($semana);
+    echo "<br>";
+    echo $semana[3];
+    echo "<br>";
+    echo $semana[6];
+    echo "<br>";
     
+    $planetas = []; #Tambien se puede usar $planetas = array();
+    $planetas [] = 'Marte';
+    $planetas [] = 'Tierra';
+    $planetas [] = 'Venus';
+
+    echo count ($planetas); // la funcion count sirve para imprimir la cantidad de elementos en pantalla 3
+
+    var_dump ($planetas);
+    */
+    
+
+
+    $semana = [
+        'Lunes',
+        'Martes',
+        'Miercoles',
+        'Jueves',
+        'Viernes',
+        'Sabado',
+        'Domingo'
+        ];
+        // foreach significa "para cada"
+        foreach ($semana as $dia) {
+            echo $dia ./* el punto concatena */ "<br>";
+        }
 
     ?>
 </body>
